0c6d9004344bc326cf90220704f5db44c8eac532
Влад Цуненко <vlad.tsunenko.2004@mail.ru>
2024-03-23 23:42:42 +0400

    added docstring and typehints

--- lab_3/HybridEncryption.py
+++ lab_3/HybridEncryption.py
@@ -9,7 +9,23 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 logging.basicConfig(level=logging.INFO)
 
 class HybridEncryption:
-    def __init__(self, text_path, private_key_path, public_key_path, symmetric_key_path, encrypted_text_path, decrypted_text_path, key_len):
+    """
+    A class for hybrid encryption using both symmetric and asymmetric keys.
+    """
+    def __init__(self, text_path: str, private_key_path: str, public_key_path: str,
+                 symmetric_key_path: str, encrypted_text_path: str, decrypted_text_path: str,
+                 key_len: int) -> None:
+        """
+        Initialize HybridEncryption object with necessary paths and key length.
+
+        :param text_path: Path to the text file.
+        :param private_key_path: Path to the private key file.
+        :param public_key_path: Path to the public key file.
+        :param symmetric_key_path: Path to the symmetric key file.
+        :param encrypted_text_path: Path to store the encrypted text.
+        :param decrypted_text_path: Path to store the decrypted text.
+        :param key_len: Length of the key.
+        """
         self.text_path = text_path
         self.private_key_path = private_key_path
         self.public_key_path = public_key_path
@@ -19,7 +35,10 @@ class HybridEncryption:
         self.key_len = key_len
 
 
-    def generate_keys(self):
+    def generate_keys(self) -> None:
+        """
+        Generate asymmetric and symmetric keys and write them to files.
+        """
         try:
             symmetric_key = os.urandom(int(self.key_len/8))
             
@@ -51,7 +70,10 @@ class HybridEncryption:
             logging.error(f"An error occurred while generating the key: {ex}")
 
 
-    def encrypt_text(self):
+    def encrypt_text(self) -> None:
+        """
+        Encrypt the text using the generated symmetric key and write it to a file.
+        """
         try:
             d_symmetric_key = self._deserialization_symmetric_key()
         
@@ -66,7 +88,10 @@ class HybridEncryption:
             logging.error(f"An error occurred while encrypting and writing text to a file: {ex}.",exc_info=True)
 
 
-    def decrypt_text(self):
+    def decrypt_text(self) -> None:
+        """
+        Decrypt the text using the generated symmetric key and write it to a file.
+        """
         try:
             d_symmetric_key = self._deserialization_symmetric_key()
             
@@ -80,7 +105,14 @@ class HybridEncryption:
             logging.error(f"An error occurred while decrypting and writing text to a file: {ex}.",exc_info=True)
 
 
-    def _encrypt_text_with_symmetric_key(self, symmetric_key, text):
+    def _encrypt_text_with_symmetric_key(self, symmetric_key: bytes, text: bytes) -> bytes:
+        """
+        Encrypts the text using the provided symmetric key.
+
+        :param symmetric_key: Symmetric key.
+        :param text: Text to encrypt.
+        :return: Encrypted text.
+        """
         iv = os.urandom(16)
         cipher = Cipher(algorithms.AES(symmetric_key), modes.CBC(iv))
         encryptor = cipher.encryptor()
@@ -89,7 +121,14 @@ class HybridEncryption:
         return iv + encryptor.update(padded_text) + encryptor.finalize()
 
 
-    def _decrypt_text_with_symmetric_key(self, symmetric_key, encrypted_text):
+    def _decrypt_text_with_symmetric_key(self, symmetric_key: bytes, encrypted_text: bytes) -> bytes:
+        """
+        Decrypts the text using the provided symmetric key.
+
+        :param symmetric_key: Symmetric key.
+        :param encrypted_text: Encrypted text.
+        :return: Decrypted text.
+        """
         iv = os.urandom(16)
         cipher = Cipher(algorithms.AES(symmetric_key), modes.CBC(iv))
         decryptor = cipher.decryptor()
@@ -99,7 +138,12 @@ class HybridEncryption:
         return unpadded_dc_text
     
     
-    def _serialization_asymmetric_keys(self, asymmetric_keys):
+    def _serialization_asymmetric_keys(self, asymmetric_keys: rsa.RSAPrivateKey) -> None:
+        """
+        Serialize asymmetric keys and write them to files.
+
+        :param asymmetric_keys: Asymmetric keys to serialize.
+        """
         with open(self.public_key_path, 'wb') as public_out:
             public_out.write(asymmetric_keys.public_key().public_bytes(encoding=serialization.Encoding.PEM,
                 format=serialization.PublicFormat.SubjectPublicKeyInfo))
@@ -111,6 +155,11 @@ class HybridEncryption:
     
     
     def _deserialization_asymmetric_keys(self) -> tuple[rsa.RSAPrivateKey, rsa.RSAPublicKey]:
+        """
+        Deserialize the asymmetric keys from their respective files.
+
+        :return: The deserialized private and public keys.
+        """
         with open(self.public_key_path, 'rb') as pem_in:
             public_bytes = pem_in.read()
             d_public_key = serialization.load_pem_public_key(public_bytes)
@@ -120,23 +169,48 @@ class HybridEncryption:
         return d_private_key, d_public_key
     
     
-    def _serialization_symmetric_key(self, encrypted_symmetric_key) -> None:
+    def _serialization_symmetric_key(self, encrypted_symmetric_key: bytes) -> None:
+        """
+        Serialize the symmetric key and save it to a file.
+
+        :param encrypted_symmetric_key: The encrypted symmetric key.
+        """
         with open(f"{self.symmetric_key_path[:-4]}_{self.key_len}.txt", 'wb') as key_file:
                     key_file.write(encrypted_symmetric_key)
     
     
     def _deserialization_symmetric_key(self) -> bytes:
+        """
+        Deserialize the symmetric key from its file and decrypt it.
+
+        :return: The decrypted symmetric key.
+        """
         with open(f"{self.symmetric_key_path[:-4]}_{self.key_len}.txt", mode='rb') as key_file: 
             encrypted_symmetric_key = key_file.read()
         d_private_key, _ = self._deserialization_asymmetric_keys()
         return d_private_key.decrypt(encrypted_symmetric_key,OAEP(mgf=MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
     
     
-    def _read_text(self, text_path, mode, encoding=None):
+    def _read_text(self, text_path: str, mode: str, encoding=None) -> str:
+        """
+        Read text from a file.
+
+        :param text_path: Path to the text file.
+        :param mode: File open mode.
+        :param encoding: Text encoding. Defaults to None.
+
+        :return: The content of the text file.
+        """
         with open(text_path, mode=mode,encoding=encoding) as text_file:
                 return text_file.read()
     
     
-    def _write_text(self, text, text_path) -> None:
+    def _write_text(self, text: bytes, text_path: str) -> None:
+        """
+        Write text to a file.
+
+        :param text: Text to be written.
+        :param text_path: Path to the text file.
+        """
         with open(text_path, "wb") as text_file:
                 text_file.write(text)
